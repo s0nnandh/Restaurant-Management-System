5c6e28eb8e9d6ba270de944b39e56c9d9bd99f54
harshitha <harshitha@cse.iitb.ac.in>
2022-04-29 08:45:30 +0530

    added place_order api

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -37,4 +37,38 @@ module.exports = {
         });
     },
 
+    placeOrder: function(req, res, next) {
+        const query_1 = `insert into order_($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) returning id`;
+        values_1 = [req.body.total_cost, req.body.payment_method, req.body.order_mode, "Ordered",
+            req.body.table_id, req.body.placing_time, req.body.customer_id, req.body.delivery_area,
+            NULL, NULL, req.body.order_date, NULL]
+        db.one(query_1, [values_1]).then(result => {
+            lst = req.body.order_items;
+            lst.forEach(element => {
+                element["order_id"] = result.order_id;
+            });
+            db.tx(t => {
+                const queries = lst.map(l => {
+                    return t.none('INSERT INTO order_items(item_id, order_id, item_quantity) VALUES(${item_id}, ${order_id}, ${item_quantity})', l);
+                });
+                return t.batch(queries);
+            })
+                .then(data => {
+                    // SUCCESS
+                    // data = array of null-s
+                    res.sendStatus(200);
+                })
+                .catch(error => {
+                    res.sendStatus(500);
+                    console.log(err);
+                    return next(err);
+                    // ERROR
+                });
+        })
+        .catch(err => {
+            console.log(err);
+            return next(err);
+        });
+    }
+
 }
